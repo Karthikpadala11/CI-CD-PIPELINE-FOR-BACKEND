7ff0a938febbe42fa33b03e07efb9e21827ba766
Karthikpadala11 <kartikpadala0@gmail.com>
2026-04-10 13:29:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const tickets = [
   { from: "Delhi", to: "Mumbai", price: 5000 },
   { from: "Tirupati", to: "Nagpur", price: 8000 },
   { from: "Nagpur", to: "Gondia", price: 2000 },
-  { from: "Nagpur", to: "Vizag", price: 8500 }
+  { from: "Nagpur", to: "Vizag", price: 7500 }
 ];
 
 // Health check endpoint (for ALB)
